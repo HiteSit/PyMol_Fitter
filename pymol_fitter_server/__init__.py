8771945fbadea714f15b02fc2bade44d9a3c49fd
Riccardo_Linux <riccardofusco99@gmail.com>
2025-04-15 11:37:36 +0200

    Git ComeBack

--- pymol_fitter_server/__init__.py
+++
@@ -1,2 +0,0 @@
-# PyMOL Docking Docker - Server Package
-# This package contains the Flask application and related utilities for the PyMOL Docking Docker plugin 
--- pymol_fitter_server/pymol_fitter_src/__init__.py
+++
@@ -1,2 +0,0 @@
-# PyMOL Docking Docker - Server-side module
-# This module contains the server-side code for the PyMOL Docking Docker plugin 
